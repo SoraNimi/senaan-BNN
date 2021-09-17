96d3e554974cbba94ab8203e52759f6557e2bf37
SoraNimi <434624198@qq.com>
2021-09-17 15:28:33 +0800

    ------fist add-----

--- bnn/mnist_mlp.py
+++ bnn/mnist_mlp.py
@@ -93,12 +93,18 @@ X_train = X_train.reshape(60000, 784)
 X_test = X_test.reshape(10000, 784)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-X_train[X_train>0]=1
-X_train[X_train==0]=-1
-X_test[X_test>0]=1
-X_test[X_test==0]=-1
+# X_train /= 255
+# X_test /= 255
+# X_train[X_train>0]=1
+# X_train[X_train==0]=-1
+# X_test[X_test>0]=1
+# X_test[X_test==0]=-1
+
+X_train[X_train>20]=1
+X_train[X_train<=0]=-1
+X_test[X_test>20]=1
+X_test[X_test<=0]=-1
+
 #print(X_train[0])
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
