08ab5cad4e3852a2c66e0318a28f76ce06372f51
OHSAWA LAB <434624198@qq.com>
2021-10-19 15:45:08 +0900

    ----------test-

--- ReRAM Array/gene_dnn_sp_by_bu40n3.py
+++ ReRAM Array/gene_dnn_sp_by_bu40n3.py
@@ -35,7 +35,7 @@ print(neuron[0].shape)
 print(neuron[2].shape)
 print(neuron[4].shape)
 print(neuron[6].shape)
-header_file = open("module_header.sp", "r")
+header_file = open("module_header.sp", "r", encoding='UTF-8')
 line = header_file.readline()
 while (not line == ""):
     output_file.write(line)
